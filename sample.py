f84a88804a85a0ec51e1bcfdad42bf0ab1263445
DRK1392 <102576200+DRK1392@users.noreply.github.com>
2023-01-23 20:45:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -17,3 +17,15 @@ print ("This is commit based job for jenkins its third commit")
 print ("This is commmit based job for jenkins its fourth commit")
 
 print ("This is commit based job for jenkins its fifth commit")
+
+print ("This is commit based job for jenkins its sixth commit")
+
+print ("This is commit based job for jenkins its seventh commit")
+
+print ("This is commit based job for jenkins its eighth commit")
+
+print ("This is commit based job for jenkins its nineth commit")
+
+print ("This is commit based job for jenkins its tenth commit")
+
+print ("This is commit based job for jenkins its leventh commit")
